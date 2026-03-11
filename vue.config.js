56221181a77d2541b102607d125fda4485f297c1
joy-aliang <13633731517@163.com>
2026-03-11 19:31:02 +0800

    调用本地防止跨域

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ module.exports = {
 	devServer: {
 		proxy: {
 			'/api': {
-				target: 'http://175.178.83.155:8889',
+				target: 'http://localhost:8889',
 				changeOrigin: true,
 				logLevel: 'debug',
 				pathRewrite: {
